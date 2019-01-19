a4d1b8c863c586336e8ff2d416ad0ede01bb03d6
Kosim <kosim.shukurov@gmail.com>
2019-01-19 12:52:51 -0500

    third chaging

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,7 +6,8 @@ public class Calculator {
     }
 
     public static double subtract(double num1, double num2) {
-        return num1 - num2;
+        double result = num1 - num2;
+        return result;
 
     }
 }
